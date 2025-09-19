ccb8ec950726ea673dc02844fbafc7c6f12754c2
Purushotham Pulakala <purushotham.pulakala@bp.com>
2025-09-19 21:02:56 +0530

    1- git initiated

---
+++ dvc_practice_1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["Alice", "Bob", "Charlie", "David", "Eva"],
+    "Age": [24, 30, 22, 35, 28],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+})
+print(df)
